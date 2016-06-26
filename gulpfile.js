24bba14173af3be3de3067978f1d0c9f3051f98c
r-okinaga <Hrnn0PDk1>
2016-06-26 18:53:12 +0900

    コンパイル元修正

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,13 @@ var sass = require("gulp-sass");
 var coffee = require("gulp-coffee");
 
 gulp.task("sass", function() {
-    gulp.src("sass/**/*scss")
+    gulp.src("src/sass/**/*scss")
         .pipe(sass())
         .pipe(gulp.dest("./www/css"));
 });
 
 gulp.task("coffee", function() {
-    gulp.src("coffee/**/*coffee")
+    gulp.src("src/coffee/**/*coffee")
         .pipe(coffee())
         .pipe(gulp.dest("./www/js"));
 });
